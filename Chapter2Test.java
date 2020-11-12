cef24a1ccf2a061f3825b9f73e7ad8dd8a6234a2
MartyWicker1 <73349378+MartyWicker@users.noreply.github.com>
2020-11-12 14:59:28 +0000

    I did the work for the required assignment

--- Chapter2Test.java
+++ Chapter2Test.java
@@ -1,7 +1,22 @@
-
+import java.util.Scanner;
 
 public class Chapter2Test {
   public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
   
+  System.out.println("Please type an integer.");
+
+  int num1 = scan.nextInt();
+
+  System.out.println("Please type a second integer.");
+
+  int num2 = scan.nextInt();
+
+  System.out.println("You chose:" + " " + num1 + " " + "and" + " " + num2 + ".");
+
+  double num3 = (num1+num2)/2.0;
+
+  System.out.println(num3);
   }
 }
